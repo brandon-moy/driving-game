f34024599114c19a92bae781754ebcaba49ef52c
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-21 21:21:15 +0000

    Adjusted moveCar function to move in any direction

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var $mach5 = document.querySelector('#mach-5');
 var countSide = 1;
-// var countVert = 1;
+var countVert = 1;
 var intervalId = null;
 var isMoving = false;
 
@@ -30,6 +30,17 @@ function startStopCar(event) {
 }
 
 function moveCar(event) {
-  $mach5.style.left = (6 * countSide) + 'px';
-  countSide++;
+  if ($mach5.className === '') {
+    $mach5.style.left = (6 * countSide) + 'px';
+    countSide++;
+  } else if ($mach5.className === 'down') {
+    $mach5.style.top = (6 * countVert) + 'px';
+    countVert++;
+  } else if ($mach5.className === 'left') {
+    $mach5.style.left = (6 * countSide) + 'px';
+    countSide--;
+  } else if ($mach5.className === 'up') {
+    $mach5.style.top = (6 * countVert) + 'px';
+    countVert--;
+  }
 }
